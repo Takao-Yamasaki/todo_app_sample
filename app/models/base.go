8399fe7c8239b0bbe17bbab26bda5e544e5fe098
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-10-14 14:59:40 +0900

    add:CreateUser

--- app/models/base.go
+++ app/models/base.go
@@ -1,15 +1,16 @@
 package models
 
 import (
+	"crypto/sha1"
 	"database/sql"
 	"fmt"
 	"log"
 	"todo_app_sample/config"
 
+	"github.com/google/uuid"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// Usersテーブルの作成
 var Db *sql.DB
 
 var err error
@@ -18,6 +19,7 @@ const (
 	tableNameUser = "users"
 )
 
+// DBの初期化するメソッド。Usersテーブルの作成を行うメソッド
 func init() {
 	Db, err = sql.Open(config.Config.SQLDriver, config.Config.DbName)
 	if err != nil {
@@ -27,9 +29,22 @@ func init() {
 	cmdU := fmt.Sprintf(`create table if not exists %s(
 		id integer primary key autoincrement,
 		uuid string not null unique,
+		name string,
 		email string,
 		password string,
 		created_at datetime)`, tableNameUser)
 
 	Db.Exec(cmdU)
+}
+
+// UUIDを作成するメソッド。おそらくセッションIDとして使う
+func createUUID() (uuidobj uuid.UUID) {
+	uuidobj, _ = uuid.NewUUID()
+	return uuidobj
+}
+
+// パスワードをハッシュ化するメソッド
+func Encrypt(plaintext string) (cryptext string) {
+	cryptext = fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
+	return cryptext
 }
