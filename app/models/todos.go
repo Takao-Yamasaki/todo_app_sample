55021c60d319c3b18b0fedbdab3d0950a0531c75
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-10-14 17:51:13 +0900

    add:GetTodos

--- app/models/todos.go
+++ app/models/todos.go
@@ -37,3 +37,25 @@ func GetTodo(id int) (todo Todo, err error) {
 
 	return todo, err
 }
+
+// Todoの全件データを取得するメソッド
+func GetTodos() (todos []Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos`
+	rows, err := Db.Query(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(&todo.ID,
+			&todo.Content,
+			&todo.UserID,
+			&todo.CreatedAt)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+	}
+	rows.Close()
+	return todos, nil
+}
