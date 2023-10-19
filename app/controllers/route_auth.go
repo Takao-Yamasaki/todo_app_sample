a8861ca85f6a8cbbf47fa0d373f229edb232e656
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-10-20 08:50:09 +0900

    add:signup

--- app/controllers/route_auth.go
+++ app/controllers/route_auth.go
@@ -1,7 +1,33 @@
 package controllers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"todo_app_sample/app/models"
+)
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "public_navbar", "signup")
+	// GETメソッドの場合
+	if r.Method == "GET" {
+		generateHTML(w, nil, "layout", "public_navbar", "signup")
+		// POSTメソッドの場合
+	} else if r.Method == "POST" {
+		// Fromの解析
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		user := models.User{
+			// signup.htmlの属性のnameを渡す
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
+			PassWord: r.PostFormValue("password"),
+		}
+		// ユーザーを登録
+		if err := user.CreateUser(); err != nil {
+			log.Fatalln(err)
+		}
+		// トップページにリダイレクト
+		http.Redirect(w, r, "/", 302)
+	}
 }
